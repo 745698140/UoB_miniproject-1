59dd6a978a5fbe6ce75ccb79c4949893da262f70
charliehpearce <charliehpearce@hotmail.co.uk>
2021-02-11 14:09:03 +0000

    Update parse_raw_lob.py

--- preprocessing/parse_raw_lob.py
+++ preprocessing/parse_raw_lob.py
@@ -2,7 +2,6 @@
 Run this for the folder containing all the DAVESON with [ ] to change 
 it to JSON that can be seralized
 """
-import json
 from os import listdir
 
 # Put path to directory containing files here
@@ -17,15 +16,16 @@ for file in files:
         print('parsing '+file)
         file_in = open(folder_dir+file,'rt')
         file_out = open(folder_dir+file[:-4]+'.json','wt')
-        file_in = file_in.read()
-        file_in = file_in.replace('\n','')
-        file_in = file_in.replace(' ','')
-        file_in = file_in.replace('["time",','{"time":')
-        file_in = file_in.replace('["bid",','"bid":')
-        file_in = file_in.replace('],["ask",',',"ask":')
-        file_in = file_in.replace(']]]]',']]},')
-        file_in = file_in.replace(']]]',']},')
-        final_file = '['+file_in[:-1]+']'
+        file_working = file_in.read()
+        file_in.close()
+        file_working = file_in.replace('\n','')
+        file_working = file_in.replace(' ','')
+        file_working = file_in.replace('["time",','{"time":')
+        file_working = file_in.replace('["bid",','"bid":')
+        file_working = file_in.replace('],["ask",',',"ask":')
+        file_working = file_in.replace(']]]]',']]},')
+        file_working = file_in.replace(']]]',']},')
+        final_file = '['+file_working[:-1]+']'
         file_out.write(final_file)
-        
+        file_out.close()
 print('Job Done')
