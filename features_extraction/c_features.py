754805d7fbae302710efe925b6cf01ee3fd39118
Charlie Pearce <charliehpearce@hotmail.co.uk>
2021-02-20 22:21:51 +0000

    made object oriented for faster coding

--- features_extraction/c_features.py
+++ features_extraction/c_features.py
@@ -1,52 +1,53 @@
 import numpy as np
 import json
 
-## Statistical features
-# Takes in a number of samples and returns the AMFD
-
 class lob:
     def __init__(self, json_lob):
         self.bid = np.array(json_lob['bid'])
         self.ask = np.array(json_lob['ask'])
         self.time = json_lob['time']
 
-def average_midprice_financial_duration(lobs):
-    prices = np.zeros(len(lobs))
-    times = np.zeros(len(lobs))
+    # Microprice, volume weighted midprice
+    def microprice(self):
+        max_bid = np.argmax(self.bid)
+        min_ask = np.argmin(self.ask)
+        microprice = (self.bid[max_bid][0]*self.ask[min_ask][1] + self.bid[max_bid][1]*self.ask[min_ask][0])/(self.ask[min_ask][1]+self.bid[max_bid][1])
+        return microprice
 
-    for i,lob in enumerate(lobs):
-        times[i] = lob['time']
-        bid = np.array(lob['bid'])
-        ask = np.array(lob['ask'])
-        max_bid = np.amax(bid)
-        min_ask = np.amax(ask)
-        prices[i] = float(min_ask-max_bid)
+    # Total quanitity of all bid ask quotes
+    def total_quantity_all_quotes(self):
+        total_bid = np.sum(self.bid[:,1])
+        total_ask = np.sum(self.ask[:,1])
+        return total_bid+total_ask
 
-    mfd = np.cumsum(times)/np.cumsum(prices)
-    amfd = np.mean(mfd)
-    return amfd
+class lobs:
+    def __init__(self, lobs):
+        self.lob_lst = [lob(lob_entry) for lob_entry in lobs]
 
-# Microprice, volume weighted midprice
-def microprice(lob):
-    bid = np.array(lob['bid'])
-    ask = np.array(lob['ask'])
-    max_bid = np.argmax(bid)
-    min_ask = np.argmin(ask)
-    microprice = (bid[max_bid][0]*ask[min_ask][1] + bid[max_bid][1]*ask[min_ask][0])/(ask[min_ask][1]+bid[max_bid][1])
-    return microprice
+    # Take in a list of lobs and calculate AMFD
+    def average_midprice_financial_duration(self):
+        prices = np.zeros(len(self.lob_lst))
+        times = np.zeros(len(self.lob_lst))
+        
+        for i,lob in enumerate(self.lob_lst):
+            times[i] = lob.time
+            max_bid = np.amax(lob.bid)
+            min_ask = np.amax(lob.ask)
+            prices[i] = float(min_ask-max_bid)
 
-# Total quanitity of all bid ask quotes
-def total_quantity_all_quotes(lob):
-    bid = np.array(lob['bid'])
-    ask = np.array(lob['ask'])
-    total_bid = np.sum(bid[:,1])
-    total_ask = np.sum(ask[:,1])
-    return total_bid+total_ask
+        mfd = np.cumsum(times)/np.cumsum(prices)
+        amfd = np.mean(mfd)
+        return amfd
 
+if __name__ == '__main__':
+    # Load sample JSON and decode into varible
+    with open('./feature_testing.json') as json_file:
+        features = json.load(json_file)
+    samples = features[1:10]
+    sample = features[1]
 
-# Load sample JSON and decode into varible
-with open('./feature_testing.json') as json_file:
-    features = json.load(json_file)
-samples = features[1:10]
-sample = features[1]
-print(microprice(sample))
+    lob_sample = lob(sample)
+    print(lob_sample.microprice())
+
+    lob_samples = lobs(samples)
+    print(lob_samples.average_midprice_financial_duration())
