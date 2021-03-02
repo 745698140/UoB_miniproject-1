c5d0e26d9fee0ec30a622aef29464e1e8db1f671
HaishuoFang <fanghaishuo@163.com>
2021-03-02 20:57:07 +0000

    add cl args

--- run_feature_extraction.sh
+++ run_feature_extraction.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-nohup python3 features_extraction/extract_features.py --start_index 0 --end_index 10
+nohup python3 features_extraction/extract_features.py --start_index 0 --end_index 10 \
+--subpath_s3bucket b_02 --batch_size 100000
 
 
