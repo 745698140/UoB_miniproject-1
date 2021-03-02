77cc53730f908920877aa686df51563dc591a555
HaishuoFang <fanghaishuo@163.com>
2021-03-02 20:48:48 +0000

    change write path

--- features_extraction/extract_features.py
+++ features_extraction/extract_features.py
@@ -14,6 +14,7 @@ from all_features import lob, lobs
 import os
 import s3fs
 import argparse
+import re
 
 
 def read_file(fs, dir, file_name):
@@ -23,7 +24,7 @@ def read_file(fs, dir, file_name):
     return string
 
 
-def get_features(json_data, num_features, time_window, batch_number, loblevel):
+def get_features(json_data, num_features, time_window, batch_number, loblevel, date, write_path):
     print(f"starting process {batch_number} files at process {os.getpid()}")
     
     # Create multiprocessing pool
@@ -38,12 +39,12 @@ def get_features(json_data, num_features, time_window, batch_number, loblevel):
         # tok = time.time()
         # print(f'time taken for processing {batch_number} is {tok-tik} s')
 
-        np.save(f'./data/features_extracted{batch_number}.npy', feature_matrix)
+        np.save(os.path.join(write_path,f'features_{date}_{batch_number}.npy'), feature_matrix)
         
         # columns=['time','microprice','total_quantity_all_quotes','average_midprice_financial_duration']
         # df = pd.DataFrame(feature_matrix,columns=columns)
         # df.to_csv(f'./data/test{batch_number}.csv')
-        print(f"finish processing {batch_number}!")
+        print(f"finish processing {date}_{batch_number}!")
 
     except Exception as error:
         raise error
@@ -93,17 +94,26 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--start_index", type=int)
     parser.add_argument("--end_index", type=int)
+    parser.add_argument("--batch_size", default=100000, type=int)
+    parser.add_argument("--processors", default=8, type=int)
+    parser.add_argument("--window_size", default=10, type=int)
+    parser.add_argument("--loblevel", default=4, type=int)
+    parser.add_argument("--subpath_s3bucket", default="b_02", type=str)
+    
     args = parser.parse_args()
-
+    pattern = re.compile("(?<=_)2022-[0-9]{2}-[0-9]{2}")
     fs = s3fs.S3FileSystem(anon=False)
     # json_string = read_file('.','data/TstB02_2022-01-04LOBs.json')
-    s3_bucket = "s3://uob-miniproject/b_02/raw"
-    file_names = load_data_from_s3(fs, s3_bucket, args.start_index, args.end_index)
+    s3_bucket = "s3://uob-miniproject"
+    json_data_path = os.path.join(s3_bucket, args.subpath_s3bucket, 'json')
+    write_path = os.path.join(s3_bucket, args.subpath_s3bucket, 'features')
+    file_names = load_data_from_s3(fs, json_data_path, args.start_index, args.end_index)
     for file in file_names:
         if '.json' not in file:
             continue
         json_string = read_file(fs, '', file)
         json_data = json.loads(json_string)
+        date = re.search(pattern, file).group()
         print('loaded json data')
 
         # json_string2 = read_file('.','data/TstB02_2022-01-04LOBs.json')
@@ -113,17 +123,17 @@ if __name__ == "__main__":
         # print("convert to df")
         # tapes = pd.read_csv("data/TstB02_2022-01-04tapes.csv", header=None)
         start = time.time()
-        batch_size = 100000
-        processes = 8
-        window_size = 10
+        batch_size = args.batch_size
+        processes = args.processors
+        window_size = args.window_size
         number_features = 12
-        loblevel=4
+        loblevel = args.loblevel
         pool = multiprocessing.Pool(processes=processes)
 
         # The first batch
-        data = [(json_data[0:batch_size], number_features, window_size, 0, loblevel)]
+        data = [(json_data[0:batch_size], number_features, window_size, 0, loblevel, date, write_path)]
         # Remaining batch needs to add the previous k data points
-        remain = [(json_data[i-window_size+1:i+batch_size], number_features, window_size, i//batch_size, loblevel) for i in range(batch_size,len(json_data), batch_size)]
+        remain = [(json_data[i-window_size+1:i+batch_size], number_features, window_size, i//batch_size, loblevel, date, write_path) for i in range(batch_size,len(json_data), batch_size)]
         data.extend(remain)
         results = pool.starmap_async(get_features, data)
         if results.get()[0]:
